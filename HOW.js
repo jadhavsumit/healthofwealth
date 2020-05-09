53df90530527011d710b9adfcee02aae918814de
jadhavsumit <smtjdhv5@gmail.com>
2020-05-09 18:33:15 +0100

    Add files via upload

---
+++ HOW.js
@@ -0,0 +1,62 @@
+
+
+//----------- Scroll to the top-----------//
+
+const scroll = document.querySelector('#scroll');
+
+scroll.addEventListener("click", function(){
+    // window.scrollTo({
+    //     top:0,
+    //     left:0,
+    //     behavior: "smooth"
+    // });
+
+    $("html,body").animate({scrollTop:0}, "slow");
+});
+
+//----- planning Scroll-----//
+function myFunction() {
+    var elmnt = document.getElementById("financial");
+    elmnt.scrollIntoView({behavior: "smooth"});
+  }
+
+// ------ modal ---- //
+
+var modal = document.getElementById("simpleModal");
+var modalBtn = document.getElementById("modalbtn");
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+var investments = document.getElementById("investment");
+var contacts = document.getElementById("contact");
+var okbtns = document.getElementById("okbtn");
+
+modalBtn.addEventListener("click", openmodal);
+
+closeBtn.addEventListener("click", closemodal);
+
+okbtns.addEventListener("click", closemodal);
+
+window.addEventListener("click", outsideclick); 
+
+investments.addEventListener('click', openmodal);
+
+contacts.addEventListener('click', openmodal);
+
+
+
+function openmodal(){
+    modal.style.display = "block";
+}
+
+function closemodal(){
+    modal.style.display = "none";
+}
+
+function outsideclick(e){
+    if(e.target == 'modal'){
+        modal.style.display = "none";
+    }
+   
+}
+
+investments.addEventListener('click', openmodal);
+contacts.addEventListener('click', openmodal);
